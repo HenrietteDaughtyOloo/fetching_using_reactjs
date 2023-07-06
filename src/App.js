c3b9bdb6e14fbb292c8edd03c12eb27aa01dc436
HenrietteDaughtyOloo <henriettedoloo@gmail.com>
2023-07-06 14:10:12 +0300

    fetching products using React

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import{Routes, Route, BrowserRouter,} from 'react-router-dom';
+import'./App.css';
+import Login from './Login';
+import Products from './Products';
+import ProductDetails from './ProductDetails';
+import AddProduct from './AddProduct';
+
+function App() {
+      return (
+    <div>
+      <BrowserRouter>
+  
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/Products" element={<Products />} />
+          <Route path="/add-product" element={<AddProduct />} />
+
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+
+  
+}
+
+export default App;
